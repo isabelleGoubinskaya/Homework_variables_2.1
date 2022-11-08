7c0542f292449eee4d9cbca4772651aaeeb8c1b0
Isabelle <isagoubinskaya@gmail.com>
2022-11-08 21:36:21 +0100

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,12 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }
