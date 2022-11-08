9112c06136b70c3749dba6ceef135d1d6b944b08
Isabelle <isagoubinskaya@gmail.com>
2022-11-08 21:09:47 +0100

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        byte dog = 8;
-        double cat = 3.6;
-        int paper = 763789;
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
