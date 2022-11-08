422ced7def71250c15bd0597f801949ef6b9e110
Isabelle <isagoubinskaya@gmail.com>
2022-11-08 21:01:19 +0100

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        byte dog = 8;
+        double cat = 3.6;
+        int paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+}
