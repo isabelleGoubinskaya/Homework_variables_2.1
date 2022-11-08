fa5f347fe531e03f4e8ee5e510b9899b90258548
Isabelle <isagoubinskaya@gmail.com>
2022-11-08 21:25:53 +0100

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import javax.swing.plaf.synth.SynthViewportUI;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
+        var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
@@ -9,6 +12,12 @@ public class Main {
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
